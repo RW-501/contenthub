d657a2947a7803c7f9d6afb19c62e51f750dfe0e
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-07-03 18:17:08 -0500

    up

--- includes/footer.js
+++ includes/footer.js
@@ -4,17 +4,3 @@
 
 document.getElementById('footerYear').textContent = new Date().getFullYear();
 
-  async function loadCollabModal() {
-    const res = await fetch('https://rw-501.github.io/contenthub/includes/collabRequestModal.html');
-    const text = await res.text();
-    const template = document.createElement('div');
-    template.innerHTML = text;
-    document.body.appendChild(template.querySelector('template').content.cloneNode(true));
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src = 'https://rw-501.github.io/contenthub/includes/collabRequestModal.js'; // put the logic part here
-    document.body.appendChild(script);
-  }
-
-  // Call this when you know it might be used
-  loadCollabModal();
