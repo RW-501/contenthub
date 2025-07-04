9cb4a0de927f0a3987b102736abb491fd9c3379a
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-07-03 19:28:26 -0500

    up

--- js/auth.js
+++ js/auth.js
@@ -13,7 +13,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 import {
-  updateDoc, doc, getDoc, setDoc
+  updateDoc, doc, getDoc, setDoc, db
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 import { app, auth } from "https://rw-501.github.io/contenthub/js/firebase-config.js";
