e0015b52056bc4214d6a206df59a4c62d1ed6123
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-07-08 12:10:35 -0500

    up

--- includes/footer.js
+++ includes/footer.js
@@ -4,3 +4,26 @@
 
 document.getElementById('footerYear').textContent = new Date().getFullYear();
 
+
+function loadPostScript() {
+    const existing = document.querySelector('script[src="https://rw-501.github.io/contenthub/includes/post.js"]');
+    if (existing) return;
+
+    const script = document.createElement('script');
+    script.src = "https://rw-501.github.io/contenthub/includes/post.js";
+    script.type = "module";  // 💥 THIS IS REQUIRED
+    document.head.appendChild(script);
+  }
+console.log(" loadPostScript post area");
+
+  // Load it when needed
+  loadPostScript();
+
+window.addEventListener("load", () => {
+  // same loadPostScript() function inside here
+  console.log("window loadPostScript");
+
+});
+  console.log("??????????????????");
+
+
--- includes/nav.js
+++ includes/nav.js
@@ -66,29 +66,6 @@ function loadAuthScript() {
 // Load it when needed
 loadAuthScript();
 
-document.addEventListener("DOMContentLoaded", () => {
-  function loadPostScript() {
-    const existing = document.querySelector('script[src="https://rw-501.github.io/contenthub/includes/post.js"]');
-    if (existing) return;
-
-    const script = document.createElement('script');
-    script.src = "https://rw-501.github.io/contenthub/includes/post.js";
-    script.type = "module";  // 💥 THIS IS REQUIRED
-    document.head.appendChild(script);
-  }
-console.log(" loadPostScript post area");
-
-  // Load it when needed
-  loadPostScript();
-});
-window.addEventListener("load", () => {
-  // same loadPostScript() function inside here
-  console.log("window loadPostScript");
-
-});
-  console.log("??????????????????");
-
-
 
 
 window.showModal = function({
