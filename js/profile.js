544d8426e9190f85205fa23d31f7b990a3ac9e90
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-07-12 12:25:01 -0500

    up

--- js/profile.js
+++ js/profile.js
@@ -2391,8 +2391,8 @@ function renderTaggedUsers(taggedUserIds) {
   }).join("");
 }
 
-  import('https://rw-501.github.io/contenthub/includes/nav.js');
-  
+  import renderBadgeTile  from ('https://rw-501.github.io/contenthub/includes/nav.js');
+
 async function loadPublicBadges(userData) {
   const badgeList = document.getElementById("badgeList");
   if (!badgeList) {
