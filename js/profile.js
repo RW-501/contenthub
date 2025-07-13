f2ecdf32dab87972418f560136f77b5791e1a6a4
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-07-13 07:38:47 -0500

    up

--- js/profile.js
+++ js/profile.js
@@ -110,7 +110,7 @@ onAuthStateChanged(auth, async user => {
   });
 
   // Profile Actions
-  const isOwnerView = actingAsUser ||  !viewingUserId || viewingUserId === actingAsUser.uid;
+  const isOwnerView = actingAsUser && !viewingUserId || actingAsUser && viewingUserId === actingAsUser.uid;
   const collabBtn = document.getElementById("collabBtn");
   const editProfileBtn = document.getElementById("editProfileBtn");
   const analyticsBtn = document.getElementById("analyticsBtn");
